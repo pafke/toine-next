bacac6f9b3d686cdc8b667a905c9ebd6ff42536d
timo.veld <timo.veld@iodigital.com>
2023-09-21 10:06:24 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import StoryblokStory from '@storyblok/react/story';
 
 export default async function Home() {
     const res = await fetch(
-        `https://api.storyblok.com/v2/cdn/stories/home?version=published&token=${process.env.ACCESS_TOKEN}`,
-        { cache: 'no-store' }
+        `https://api.storyblok.com/v2/cdn/stories/home?version=published&token=${
+            process.env.ACCESS_TOKEN
+        }&cv=${new Date().getTime()}`
     );
     const data = await res.json();
     return (
