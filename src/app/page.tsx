8eeead85f14f6f1ca7a9e9dfbe4d02b550984cd2
timo.veld <timo.veld@iodigital.com>
2023-09-21 10:16:06 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
+import { getStoryblokApi } from '@storyblok/react/rsc';
 import StoryblokStory from '@storyblok/react/story';
 
+async function fetchData() {
+    const currentDate = new Date();
+    const timestamp = currentDate.getTime();
+    let sbParams: {
+        version: 'draft' | 'published' | undefined;
+        cv: number;
+    } = { version: 'published', cv: timestamp };
+
+    const storyblokApi = getStoryblokApi();
+    return storyblokApi.get(`cdn/stories/home`, sbParams);
+}
+
 export default async function Home() {
-    const res = await fetch(
-        `https://api.storyblok.com/v2/cdn/stories/home?version=published&token=${
-            process.env.ACCESS_TOKEN
-        }&cv=${new Date().getTime()}`
-    );
-    const data = await res.json();
+    const { data } = await fetchData();
     return (
         <div>
             <StoryblokStory story={data.story} />
